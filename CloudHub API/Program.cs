1f728bb126cbbdcab906f71cec1947c9091061e2
Martin  Stoyanov <martotbb.2556@gmail.com>
2026-06-09 11:20:32 +0300

    Initial commit

---
+++ CloudHub API/Program.cs
@@ -0,0 +1,27 @@
+using Amazon.EC2;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var awsOptions = builder.Configuration.GetAWSOptions();
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddDefaultAWSOptions(awsOptions);
+builder.Services.AddAWSService<IAmazonEC2>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

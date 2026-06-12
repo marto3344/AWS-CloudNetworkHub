14609b2b4351534b8cab5d17d456a8aa49bdd910
Martin  Stoyanov <martotbb.2556@gmail.com>
2026-06-12 07:50:31 +0300

    added security audit controller

---
+++ CloudHub API/Controllers/SecurityAuditController.cs
@@ -0,0 +1,39 @@
+﻿using CloudHub_API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudHub_API.Controllers;
+
+[ApiController]
+[Route("api/security")]
+public class SecurityAuditController (ISecurityAuditService securityAuditService, ILogger<SecurityAuditController> logger) : ControllerBase
+{
+    [HttpGet("vpcs")]
+    public async Task<IActionResult> GetVpcs()
+    {
+        try
+        {
+            var vpcs = await securityAuditService.GetVpcsAsync();
+            return Ok(vpcs);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error in GetVpcs endpoint");
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet("audit")]
+    public async Task<IActionResult> GetSecurityAudit()
+    {
+        try
+        {
+            var audit = await securityAuditService.GetSecurityAuditAsync();
+            return Ok(audit);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error in GetSecurityAudit endpoint");
+            return StatusCode(500, e.Message);
+        }
+    }
+}

f9bf1d9437c005562b5bbff137a0ae7ff225b220
Martin  Stoyanov <martotbb.2556@gmail.com>
2026-06-13 23:56:03 +0300

    Created Client project

---
+++ CloudHubClient/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CloudHubClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!) });
+
+await builder.Build().RunAsync();
